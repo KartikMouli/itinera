d2a3d6bd508f409b49842b1a49fc10de76899c7e
KartikMouli <kartikmouli0@gmail.com>
2025-04-28 03:28:23 +0530

    feat:add layout for authenticated pages and public pages

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -3,24 +3,23 @@ import Navbar from "@/components/navbar/navbar"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { cookies } from "next/headers"
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
+export default async function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
     const cookieStore = await cookies()
     const defaultOpen = cookieStore.get("sidebar_state")?.value === "true"
     return (
         <SidebarProvider defaultOpen={defaultOpen}>
             <div className="flex min-h-screen w-full">
                 <AppSidebar />
-                <div className="flex flex-col w-full">
-                    <div className="flex justify-between items-center px-5">
+                <div className="flex-1 flex flex-col">
+                    <div className="flex w-full justify-between items-center px-8">
                         <SidebarTrigger />
                         <Navbar />
                     </div>
-                    <main className="w-full">
+                    <main className="flex-1 p-6">
                         {children}
                     </main>
                 </div>
             </div>
-
         </SidebarProvider>
     )
-}
+} 
