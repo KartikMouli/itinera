26b57b6252434693f91f94fd662dcb37311f0d56
KartikMouli <kartikmouli0@gmail.com>
2025-05-14 00:09:10 +0530

    hotfix: one more github issues fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -18,7 +18,7 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }
     },
-    trustedOrigins: [process.env.BETTER_AUTH_URL as string],
+    trustedOrigins: ["https://itinera-six.vercel.app"],
     plugins: [ 
         username() 
     ] 
