5d8e42941ffdb8807bba5d453a9aa253d72800b3
KartikMouli <kartikmouli0@gmail.com>
2025-05-14 00:02:00 +0530

    hotfix:add trusted origin cause someone on redit said so

--- lib/auth.ts
+++ lib/auth.ts
@@ -18,6 +18,7 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }
     },
+    trustedOrigins: [process.env.BETTER_AUTH_URL as string],
     plugins: [ 
         username() 
     ] 
